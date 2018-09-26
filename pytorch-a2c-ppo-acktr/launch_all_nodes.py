8081ff67c00f8812cf76b4e4f0d9e71a6a55bb60
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-09-25 22:35:26 -0400

    Modified launch script

--- pytorch-a2c-ppo-acktr/launch_all_nodes.py
+++ pytorch-a2c-ppo-acktr/launch_all_nodes.py
@@ -13,8 +13,8 @@ for row in node_list:
         'ssh',
         '-oStrictHostKeyChecking=no',
         'maxime@' + node_addr,
-        #'cd gym-miniworld/pytorch-a2c-ppo-acktr; nohup ./launch_runs.sh > /dev/null 2>&1 &',
-        'pkill -9 python3',
+        'cd gym-miniworld/pytorch-a2c-ppo-acktr; nohup ./launch_runs.sh > /dev/null 2>&1 &',
+        #'pkill -9 python3',
     ]
 
     subprocess.check_call(cmd)
