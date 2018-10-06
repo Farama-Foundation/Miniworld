1bdd67df0a139aa4a1851a62df1bb3c4f49610c2
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-10-06 17:26:28 -0400

    Adjusted num mini batches parameter

--- pytorch-a2c-ppo-acktr/arguments.py
+++ pytorch-a2c-ppo-acktr/arguments.py
@@ -33,8 +33,8 @@ def get_args():
                         help='number of forward steps in A2C (default: 5)')
     parser.add_argument('--ppo-epoch', type=int, default=4,
                         help='number of ppo epochs (default: 4)')
-    parser.add_argument('--num-mini-batch', type=int, default=32,
-                        help='number of batches for ppo (default: 32)')
+    parser.add_argument('--num-mini-batch', type=int, default=16,
+                        help='number of batches for ppo')
     parser.add_argument('--clip-param', type=float, default=0.2,
                         help='ppo clip parameter (default: 0.2)')
     parser.add_argument('--log-interval', type=int, default=10,
