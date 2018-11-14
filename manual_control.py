099fe2c82507c9bbb7f2015fbbea909288244b99
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-11-14 14:02:00 -0500

    Revert "Added action repeat to manual_control.py"
    This reverts commit 90e926c17ad96fd4bb75a071901fd0a915ee8d45.

--- manual_control.py
+++ manual_control.py
@@ -47,44 +47,32 @@ def on_key_press(symbol, modifiers):
         sys.exit(0)
 
     action = None
-    num_repeat = 1
-
-    if symbol == key.UP:
-        action = env.actions.move_forward
-        if key_handler[key.LSHIFT] or key_handler[key.RSHIFT]:
-            num_repeat = 3
-    elif symbol == key.DOWN:
-        action = env.actions.move_back
-        if key_handler[key.LSHIFT] or key_handler[key.RSHIFT]:
-            num_repeat = 3
-    elif symbol == key.LEFT:
+    if symbol == key.LEFT:
         action = env.actions.turn_left
     elif symbol == key.RIGHT:
         action = env.actions.turn_right
+    elif symbol == key.UP:
+        action = env.actions.move_forward
+    elif symbol == key.DOWN:
+        action = env.actions.move_back
     elif symbol == key.PAGEUP or symbol == key.P:
         action = env.actions.pickup
     elif symbol == key.PAGEDOWN or symbol == key.D:
         action = env.actions.drop
 
     if action != None:
-        for _ in range(num_repeat):
-            obs, reward, done, info = env.step(action)
-            #print('step_count = %s, reward=%.2f' % (env.unwrapped.step_count, reward))
+        obs, reward, done, info = env.step(action)
+        #print('step_count = %s, reward=%.2f' % (env.unwrapped.step_count, reward))
 
-            if done:
-                print('done! reward={:.2f}'.format(reward))
-                env.reset()
-                break
+        if done:
+            print('done! reward={:.2f}'.format(reward))
+            env.reset()
 
         env.render('pyglet')
 
 @env.unwrapped.window.event
 def on_close():
     pyglet.app.exit()
-
-# Register a keyboard handler
-key_handler = key.KeyStateHandler()
-env.unwrapped.window.push_handlers(key_handler)
 
 # Enter main event loop
 pyglet.app.run()
