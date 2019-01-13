424062bf8e73006f69e2848f41d313fe33dea689
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-01-12 23:12:00 -0500

    Fixed up script

--- experiments/pos_est.py
+++ experiments/pos_est.py
@@ -71,29 +71,28 @@ print('num actions:', num_actions)
 max_steps = env.max_episode_steps
 print('max episode steps:', max_steps)
 
-num_demos = 128
+max_demos = 8192
+num_demos = 0
 cur_idx = 0
 
 # Done indicates that we become done after the current step
-obss = np.zeros(shape=(num_demos, max_steps, 3, 60, 80), dtype=np.uint8)
-poss = np.zeros(shape=(num_demos, max_steps, 3), dtype=np.float32)
-active = np.zeros(shape=(num_demos, max_steps, 1), dtype=np.float32)
-
-
-
+obss = np.zeros(shape=(max_demos, max_steps, 3, 60, 80), dtype=np.uint8)
+poss = np.zeros(shape=(max_demos, max_steps, 3), dtype=np.float32)
+active = np.zeros(shape=(max_demos, max_steps, 1), dtype=np.float32)
 
 def gen_trajs(num_episodes=128):
-    global cur_idx
+    global cur_idx, num_demos
 
     for i in range(num_episodes):
         #print(i)
+
+        active[cur_idx, :] = 0
 
         obs = env.reset()
 
         start_pos = env.agent.pos
 
         for step_idx in range(max_steps):
-
             obs = obs.transpose(2, 0, 1)
 
             pos = env.agent.pos
@@ -102,7 +101,6 @@ def gen_trajs(num_episodes=128):
             obss[cur_idx, step_idx] = obs
             poss[cur_idx, step_idx] = rel_pos
             active[cur_idx, step_idx] = 1
-            cur_idx = (cur_idx + 1) % num_demos
 
             # TODO: bias towards forward movement
             # or repetition of actions for multiple time steps
@@ -113,9 +111,8 @@ def gen_trajs(num_episodes=128):
             if done:
                 break
 
-
-
-
+        cur_idx = (cur_idx + 1) % max_demos
+        num_demos = max(num_demos, cur_idx)
 
 
 model = Model(num_actions)
@@ -123,23 +120,21 @@ model.cuda()
 
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-
-
-
-
-
-batch_size = 8
+batch_size = 32
 
 running_loss = 0
 
-gen_trajs(num_demos)
+gen_trajs(batch_size)
 
 # For each batch
 for batch_idx in range(50000):
     print('batch #{}'.format(batch_idx+1))
 
     # Select a valid demo index in function of the batch size
-    demo_idx = np.random.randint(0, num_demos - batch_size)
+    demo_idx = np.random.randint(0, num_demos - batch_size + 1)
+
+    print('num_demos={}'.format(num_demos))
+    print('demo_idx={}'.format(demo_idx))
 
     # Get the observations, actions and done flags for this batch
     obs_batch = obss[demo_idx:(demo_idx+batch_size)]
@@ -153,10 +148,6 @@ for batch_idx in range(50000):
     # Create initial memory for the model
     memory = Variable(torch.zeros([batch_size, 128])).cuda()
 
-
-
-
-
     total_loss = 0
     total_steps = 0
 
@@ -166,6 +157,7 @@ for batch_idx in range(50000):
         active_step = active_batch[:, step_idx, :]
 
         if active_step.sum().item() == 0:
+            print('break at step', step_idx)
             break
 
         obs_step = obs_batch[:, step_idx]
@@ -178,12 +170,12 @@ for batch_idx in range(50000):
         total_loss += pos_loss
         total_steps += active_step.sum().item()
 
-    # Generate new data
-    gen_trajs(batch_size)
-
     optimizer.zero_grad()
     total_loss.backward()
     optimizer.step()
+
+    # Generate new data
+    gen_trajs(4)
 
     mean_loss = total_loss.item() / total_steps
 
