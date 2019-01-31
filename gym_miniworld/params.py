1c786fd7166e39622cff035d6c16508b687a5566
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-01-31 15:02:51 -0500

    Added lateral drift noise parameter for domain randomization

--- gym_miniworld/params.py
+++ gym_miniworld/params.py
@@ -95,6 +95,7 @@ DEFAULT_PARAMS.set('light_color', [0.7, 0.7, 0.7], [0.45, 0.45, 0.45], [0.8, 0.8
 DEFAULT_PARAMS.set('light_ambient', [0.45, 0.45, 0.45], [0.35, 0.35, 0.35], [0.55, 0.55, 0.55])
 DEFAULT_PARAMS.set('obj_color_bias', [0,0,0], [-0.2,-0.2,-0.2], [0.2,0.2,0.2])
 DEFAULT_PARAMS.set('forward_step', 0.15, 0.12, 0.17)
+DEFAULT_PARAMS.set('forward_drift', 0, -0.05, 0.05)
 DEFAULT_PARAMS.set('turn_step', 15, 10, 20)
 DEFAULT_PARAMS.set('bot_radius', 0.4, 0.38, 0.42)
 DEFAULT_PARAMS.set('cam_pitch', 0, -5, 5)
