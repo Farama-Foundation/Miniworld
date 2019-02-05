6f5d1538d3ef63223c357fe6200d2a81de1154c2
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-02-05 16:12:41 -0500

    Modified autoencoder to use same conv layers as pos_delta

--- experiments/autoenc.py
+++ experiments/autoenc.py
@@ -25,73 +25,64 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.encoder = nn.Sequential(
+        self.obs_to_enc = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(64, 64, kernel_size=5, stride=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(64, 64, kernel_size=4, stride=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+
             #Print(),
+            Flatten(),
 
-            nn.Conv2d(3, 32, kernel_size=5, stride=2),
-            nn.BatchNorm2d(32),
+            nn.Linear(2240, 512),
             nn.LeakyReLU(),
 
-            nn.Conv2d(32, 32, kernel_size=5, stride=2),
-            nn.BatchNorm2d(32),
+            nn.Linear(512, 2240),
             nn.LeakyReLU(),
-
-            nn.Conv2d(32, 32, kernel_size=4, stride=2),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(),
-
-            nn.Dropout(0.2),
         )
 
         self.decoder = nn.Sequential(
             #Print(),
 
-            nn.ConvTranspose2d(32, 32, kernel_size=6, stride=2),
+            nn.ConvTranspose2d(64, 64, kernel_size=6, stride=2),
             #nn.BatchNorm2d(32),
             nn.LeakyReLU(),
 
-            nn.ConvTranspose2d(32, 32, kernel_size=6, stride=2),
+            nn.ConvTranspose2d(64, 64, kernel_size=6, stride=2),
             #nn.BatchNorm2d(32),
             nn.LeakyReLU(),
 
-            nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2),
+            nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2),
             #nn.BatchNorm2d(32),
             nn.LeakyReLU(),
 
-            nn.ConvTranspose2d(32, 3, kernel_size=2, stride=1),
+            nn.ConvTranspose2d(64, 3, kernel_size=2, stride=1),
             #nn.BatchNorm2d(32),
             nn.LeakyReLU(),
         )
-
-        """
-        self.enc_to_out = nn.Sequential(
-            nn.Linear(32 * 5 * 8, 256),
-            nn.LeakyReLU(),
-            nn.Linear(256, 256),
-            nn.LeakyReLU(),
-            nn.Linear(256, 32),
-            nn.LeakyReLU(),
-            nn.Linear(32, 2)
-        )
-        """
 
         self.apply(init_weights)
 
     def forward(self, img):
         img = img / 255
 
-        x = self.encoder(img)
+        x = self.obs_to_enc(img)
 
         #print(x.size())
-        #x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), 64, 7, 5)
         #print(x.size())
-        #out = self.enc_to_out(x)
-        #return out
 
         #print(x.size())
         y = self.decoder(x)
 
-        #print(y.size())
+        print(y.size())
         y = 255 * y[:, :, 2:82, 3:63]
         #print(y.size())
 
@@ -118,7 +109,7 @@ if __name__ == "__main__":
     #parser.add_argument('--map-name', required=True)
     #args = parser.parse_args()
 
-    env = gym.make('MiniWorld-SimToReal1-v0')
+    env = gym.make('MiniWorld-SimToRealOdo-v0')
     env.domain_rand = True
     env = PyTorchObsWrapper(env)
 
@@ -132,7 +123,9 @@ if __name__ == "__main__":
         gen_data()
 
     for i in range(10000000):
-        print(i, cur_gen_idx)
+        print('batch #{}'.format(i+1))
+
+        #print(i, cur_gen_idx)
 
         batch_idx = np.random.randint(0, idx_avail - batch_size)
         batch = buffer[batch_idx:(batch_idx+batch_size)]
