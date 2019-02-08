98a41b93104fc84a0c8b1e70ca75612cc9fe0472
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-02-08 14:55:36 -0500

    Added code to plot trajectory in 2D

--- manual_control_odo.py
+++ manual_control_odo.py
@@ -14,19 +14,21 @@ from pyglet import clock
 import numpy as np
 import gym
 import gym_miniworld
-
 import torch
 from experiments.pos_delta_6chan import Model
 from experiments.utils import make_var, save_img
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env-name', default='MiniWorld-SimToRealOdo-v0')
 parser.add_argument('--domain-rand', action='store_true', help='enable domain randomization')
-parser.add_argument('--no-time-limit', action='store_true', help='ignore time step limits')
+parser.add_argument('--no-time-limit', action='store_true', default=True, help='ignore time step limits')
 parser.add_argument('--save-imgs', action='store_true', help='save images')
 args = parser.parse_args()
 
 img_idx = 0
+
+poss = []
 
 env = gym.make(args.env_name)
 
@@ -37,7 +39,10 @@ if args.domain_rand:
 
 def reset_env():
     global prev_obs
+    global poss
     prev_obs = env.reset()
+    dir = env.agent.dir if hasattr(env, 'agent') else 0
+    poss = [(np.array([0, 0, 0]), dir)]
 
 reset_env()
 
@@ -49,9 +54,36 @@ model.load_state_dict(torch.load('pos_delta.torch'))
 model.eval()
 model.cuda()
 
+def angle_to_dv(a):
+    """
+    Vector pointing in the direction of forward movement
+    """
+
+    x = math.cos(a)
+    z = -math.sin(a)
+    return np.array([x, 0, z])
+
+def angle_to_rv(a):
+    """
+    Vector pointing to the right of the agent
+    """
+
+    x = math.sin(a)
+    z = math.cos(a)
+    return np.array([x, 0, z])
+
+fig = plt.figure()
+axe = fig.add_subplot(111)
+axe.set_xlim(-1.2, 1.2)
+axe.set_ylim(-1.2, 1.2)
+sp, = axe.plot([],[], label='toto', markersize=4, color='k', marker='o', ls='')
+fig.show()
+fig.canvas.draw()
+
 def step(action):
     global prev_obs
     global img_idx
+    global poss
 
     print('step {}: {}'.format(env.step_count, env.actions(action).name))
 
@@ -79,6 +111,21 @@ def step(action):
     print()
 
     prev_obs = obs
+
+    # Plot the motion in 2D
+    p, a = poss[-1]
+    d = angle_to_dv(a)
+    r = angle_to_rv(a)
+
+    dd, dr, da = posd
+    p = p + d*dd + r*dr
+    a += da
+    poss += [(p, a)]
+
+    xs = [ p[2] for p,a in poss ]
+    ys = [ p[0] for p,a in poss ]
+    sp.set_data(xs, ys)
+    fig.canvas.draw()
 
     if args.save_imgs:
         save_img('img_{:03d}.png'.format(img_idx), obs)
