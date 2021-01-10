e11df129b46e22d0f2a2121971e11a3b9d578f7e
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-01-09 20:59:20 -0500

    Update pip package to include meshes and textures

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='gym_miniworld',
-    version='2018.8.1',
+    version='2020.1.9',
     keywords='environment, agent, rl, openaigym, openai-gym, gym, robotics, 3d',
     packages=['gym_miniworld', 'gym_miniworld.envs'],
     install_requires=[
         'gym>=0.9.0',
         'numpy>=1.10.0',
         'pyglet',
-        #'pyyaml>=3.12',
-        #'pybullet>=2.1.0',
-    ]
+    ],
+    # Include textures and meshes in the package
+    include_package_data=True
 )
